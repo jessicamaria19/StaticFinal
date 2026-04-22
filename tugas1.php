8d1198a72ff04ce33ee374011f48faa1c2766869
jessica <jessicamaria1917@gmail.com>
2026-04-22 21:17:09 +0700

    after

--- tugas1.php
+++ tugas1.php
@@ -8,12 +8,21 @@ class Pengunjung {
         self::$jumlah++;
     }
     
+    public static function reset() {
+        self::$jumlah = 0;
+    }
 }
 
-$p1 = new Pengunjung();
-$p2 = new Pengunjung();
-$p3 = new Pengunjung();
+$p1 = new Pengunjung(1);
+$p2 = new Pengunjung(2);
+$p3 = new Pengunjung(3);
+$p3 = new Pengunjung(4);
+$p3 = new Pengunjung(5);
 
 echo "Jumlah Pengunjung: " . Pengunjung::$jumlah;
+
+Pengunjung::reset();
+
+echo "<br>Jumlah setelah reset: " . Pengunjung::$jumlah;
 
 ?>
